2c2a5ddf774c01e8f47fc25c8a6594e886c5e0a1
Christian Haas <christian.haas@sevensuns.at>
2018-06-03 20:55:31 +0200

    added failure state to notify user

--- editor/project/AddManifestEntryWaitingState.go
+++ editor/project/AddManifestEntryWaitingState.go
@@ -2,10 +2,10 @@ package project
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/inkyblackness/hacked/ss1/resource"
 	"github.com/inkyblackness/hacked/ss1/resource/lgres"
@@ -14,16 +14,24 @@ import (
 )
 
 type addManifestEntryWaitingState struct {
-	view *View
+	view        *View
+	failureTime time.Time
 }
 
-func (state addManifestEntryWaitingState) Render() {
+func (state *addManifestEntryWaitingState) Render() {
 	if imgui.BeginPopupModalV("Add static world data", nil,
-		imgui.WindowFlagsNoResize|imgui.WindowFlagsNoMove|imgui.WindowFlagsNoSavedSettings) {
+		imgui.WindowFlagsNoResize|imgui.WindowFlagsNoMove|imgui.WindowFlagsNoSavedSettings|imgui.WindowFlagsAlwaysAutoResize) {
 
-		imgui.TextUnformatted(`Waiting for folders/files.
-
-From your file browser drag'n'drop the folder (or files)
+		imgui.TextUnformatted("Waiting for folders/files.")
+		if !state.failureTime.IsZero() {
+			imgui.PushStyleColor(imgui.StyleColorText, imgui.Vec4{X: 1, Y: 0, Z: 0, W: 1})
+			imgui.TextUnformatted("Previous attempt failed, no usable data detected.\nPlease check and try again.")
+			imgui.PopStyleColor()
+			if time.Now().Sub(state.failureTime).Seconds() > 5 {
+				state.failureTime = time.Time{}
+			}
+		}
+		imgui.TextUnformatted(`From your file browser drag'n'drop the folder (or files)
 of the static data you want to reference into the editor window.
 Typically, you would use the main "data" directory of the game
 (where all the .res files are).
@@ -85,7 +93,7 @@ func (staging *fileStaging) stage(name string, enterDir bool) {
 	}
 }
 
-func (state addManifestEntryWaitingState) HandleFiles(names []string) {
+func (state *addManifestEntryWaitingState) HandleFiles(names []string) {
 	staging := fileStaging{
 		resources: make(map[string]resource.Provider),
 		savegames: make(map[string]resource.Provider),
@@ -107,8 +115,6 @@ func (state addManifestEntryWaitingState) HandleFiles(names []string) {
 		state.view.requestAddManifestEntry(entry)
 		state.view.fileState = &idlePopupState{}
 	} else {
-		// TODO: add failed state, notifying failure
-		fmt.Printf("Failed...%d files (%d savegames)\n", staging.failedFiles, len(staging.savegames))
-		state.view.fileState = &idlePopupState{}
+		state.failureTime = time.Now()
 	}
 }
