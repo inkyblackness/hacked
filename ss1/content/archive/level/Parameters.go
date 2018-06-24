238a1ef58c7fa61bc425de2fddb4a4a942835f50
Christian Haas <christian.haas@sevensuns.at>
2018-06-24 10:24:17 +0200

    added parameters

---
+++ ss1/content/archive/level/Parameters.go
@@ -0,0 +1,31 @@
+package level
+
+const (
+	// ParametersSize is the serialized size, in bytes, of the Parameters structure.
+	ParametersSize = 94
+)
+
+// Parameters describes level-global properties.
+// It also contains information on the auto-maps, which, as per original code, should have been in the game state.
+type Parameters struct {
+	Size int16
+	_    [2]byte
+
+	CeilingHazardLevel   byte
+	FloorHazardLevel     byte
+	FloorHazardIsGravity byte
+	FloorHazardOn        byte
+	CeilingHazardOn      byte
+
+	_ uint32
+	_ [3][27]byte
+}
+
+// DefaultParameters returns a new instance of parameters.
+func DefaultParameters() Parameters {
+	return Parameters{
+		Size:            ParametersSize,
+		FloorHazardOn:   1,
+		CeilingHazardOn: 1,
+	}
+}
