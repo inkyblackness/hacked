74f5663e856ce410d0b64a8f38644795b71f1c13
Christian Haas <christian.haas@sevensuns.at>
2018-06-24 10:36:04 +0200

    added loop config entry

---
+++ ss1/content/archive/level/LoopConfigEntry.go
@@ -0,0 +1,8 @@
+package level
+
+const (
+	// LoopConfigEntryCount describes how many loop entries a level has.
+	LoopConfigEntryCount = 64
+	// LoopConfigEntrySize describes the size, in bytes, of a loop config entry.
+	LoopConfigEntrySize = 15
+)
