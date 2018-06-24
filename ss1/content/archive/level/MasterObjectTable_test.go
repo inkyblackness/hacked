efa27202dd13433daadb7d52b5a1de8b5b8953bc
Christian Haas <christian.haas@sevensuns.at>
2018-06-24 20:55:09 +0200

    added test for serialization

--- ss1/content/archive/level/MasterObjectTable_test.go
+++ ss1/content/archive/level/MasterObjectTable_test.go
@@ -1,10 +1,12 @@
 package level_test
 
 import (
+	"bytes"
 	"encoding/binary"
 	"testing"
 
 	"github.com/inkyblackness/hacked/ss1/content/archive/level"
+	"github.com/inkyblackness/hacked/ss1/serial"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -13,4 +15,13 @@ func TestMasterObjectEntrySerializedSize(t *testing.T) {
 	var entry level.MasterObjectEntry
 	size := binary.Size(&entry)
 	assert.Equal(t, level.MasterObjectEntrySize, size)
+}
+
+func TestMasterObjectTableCanBeSerializedWithCoder(t *testing.T) {
+	table := level.MasterObjectTable([]level.MasterObjectEntry{{}, {}})
+	buf := bytes.NewBuffer(nil)
+	encoder := serial.NewEncoder(buf)
+	encoder.Code(table)
+	data := buf.Bytes()
+	assert.Equal(t, level.MasterObjectEntrySize*2, len(data))
 }
