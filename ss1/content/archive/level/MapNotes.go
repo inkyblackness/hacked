6755980e98afcaf375482aeb74a91e6e9653702a
Christian Haas <christian.haas@sevensuns.at>
2018-06-24 10:26:56 +0200

    added map notes

---
+++ ss1/content/archive/level/MapNotes.go
@@ -0,0 +1,9 @@
+package level
+
+const (
+	// MapNotesSize is the size, in bytes, of the map notes resource.
+	MapNotesSize = 0x0800
+)
+
+// MapNotesPointer is an offset into the map notes resource.
+type MapNotesPointer uint32
