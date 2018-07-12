9ca00f299f2721b08293f30421bcbed169fe5670
Christian Haas <christian.haas@sevensuns.at>
2018-07-12 21:00:31 +0200

    added conversion to degrees

--- ss1/content/archive/level/RotationUnit.go
+++ ss1/content/archive/level/RotationUnit.go
@@ -2,3 +2,8 @@ package level
 
 // RotationUnit describes a rotation in the range 0..255.
 type RotationUnit byte
+
+// ToDegrees returns the value converted to degrees [0..360).
+func (unit RotationUnit) ToDegrees() float64 {
+	return (float64(unit) * 360.0) / 256.0
+}
