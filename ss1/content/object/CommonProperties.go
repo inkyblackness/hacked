0fdd9c947e3ecd98f27fd1a937feade8a1dea743
Christian Haas <christian.haas@sevensuns.at>
2018-07-13 21:36:54 +0200

    created CommonProperties structure

---
+++ ss1/content/object/CommonProperties.go
@@ -0,0 +1,27 @@
+package object
+
+const (
+	// CommonPropertiesSize specifies, in bytes, the length a common properties structure has.
+	CommonPropertiesSize = 27
+)
+
+// CommonProperties are generic ones available for all objects.
+type CommonProperties struct {
+	Mass          int32
+	Hitpoints     int16
+	Armor         byte
+	RenderType    byte
+	PhysicsModel  byte
+	Hardness      byte
+	Pep           byte
+	PhysicsXR     byte
+	PhysicsY      byte
+	PhysicsZ      byte
+	Resistances   uint32
+	DefenseValue  byte
+	Toughness     byte
+	Flags         uint16
+	MfdId         uint16
+	Bitmap3D      uint16
+	DestroyEffect byte
+}
--- ss1/content/object/Constants.go
+++ ss1/content/object/Constants.go
@@ -4,8 +4,5 @@ const (
 	// ClassCount describes how many object classes there are.
 	ClassCount = 15
 
-	// CommonPropertiesSize specifies, in bytes, the length a common properties structure has.
-	CommonPropertiesSize = 27
-
 	propertiesFileVersion uint32 = 0x0000002D
 )
