7a2580fb2ebbfdf5503a841d4b50c7b724accbdf
Christian Haas <christian.haas@sevensuns.at>
2018-07-20 20:31:14 +0200

    added TextureImage

---
+++ editor/render/TextureImage.go
@@ -0,0 +1,39 @@
+package render
+
+import (
+	"math"
+
+	"github.com/inkyblackness/hacked/editor/graphics"
+	"github.com/inkyblackness/hacked/ss1/resource"
+	"github.com/inkyblackness/imgui-go"
+)
+
+// TextureImage renders an image centered and fitted within the given size.
+func TextureImage(label string, cache *graphics.TextureCache, key resource.Key, size imgui.Vec2) {
+	textureID := TextureIDForBitmapTexture(key)
+
+	imgui.PushStyleColor(imgui.StyleColorChildBg, imgui.Vec4{X: 0, Y: 0, Z: 0, W: 1})
+	imgui.PushStyleVarVec2(imgui.StyleVarWindowPadding, imgui.Vec2{X: 0, Y: 0})
+	if imgui.BeginChildV(label, size, false,
+		imgui.WindowFlagsNoNav|imgui.WindowFlagsNoInputs|imgui.WindowFlagsNoScrollWithMouse|
+			imgui.WindowFlagsNoScrollbar) {
+		texture, err := cache.Texture(key)
+		if err == nil {
+			var uv imgui.Vec2
+			uv.X, uv.Y = texture.UV()
+			width, height := texture.Size()
+
+			scaleFactor := float32(math.Min(float64(size.X/width), float64(size.Y/height)))
+			imageSize := imgui.Vec2{X: width * scaleFactor, Y: height * scaleFactor}
+
+			bufferSize := imgui.Vec2{X: float32((size.X - imageSize.X) / 2), Y: float32((size.Y - imageSize.Y) / 2)}
+			imgui.SetCursorPos(bufferSize)
+
+			imgui.ImageV(textureID, imageSize, imgui.Vec2{}, uv,
+				imgui.Vec4{X: 1, Y: 1, Z: 1, W: 1}, imgui.Vec4{X: 0, Y: 0, Z: 0, W: 0})
+		}
+	}
+	imgui.EndChild()
+	imgui.PopStyleVar()
+	imgui.PopStyleColor()
+}
