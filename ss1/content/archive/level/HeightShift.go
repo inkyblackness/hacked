b90dc166ebd0791efd64aca2ab656cbada74c416
Christian Haas <christian.haas@sevensuns.at>
2018-07-23 21:52:09 +0200

    made height values round numbers again

--- ss1/content/archive/level/HeightShift.go
+++ ss1/content/archive/level/HeightShift.go
@@ -7,18 +7,18 @@ type HeightShift int32
 
 // ValueFromTileHeight returns a floating point value in tiles based on a tile height.
 func (shift HeightShift) ValueFromTileHeight(raw TileHeightUnit) (float32, error) {
-	return shift.valueFromScale(float32(raw), float32(TileHeightUnitMax))
+	return shift.valueFromScale(float32(raw), float64(TileHeightUnitMax))
 }
 
 // ValueFromObjectHeight returns a floating point value in tiles based on an object height.
 func (shift HeightShift) ValueFromObjectHeight(raw HeightUnit) (float32, error) {
-	return shift.valueFromScale(float32(raw), float32(0xFF))
+	return shift.valueFromScale(float32(raw), float64(0x100))
 }
 
-func (shift HeightShift) valueFromScale(raw float32, scale float32) (float32, error) {
-	tileHeights := []float32{32.0, 16.0, 8.0, 4.0, 2.0, 1.0, 0.5, 0.25}
+func (shift HeightShift) valueFromScale(raw float32, scale float64) (float32, error) {
+	tileHeights := []float64{32.0, 16.0, 8.0, 4.0, 2.0, 1.0, 0.5, 0.25}
 	if (shift < 0) || (int(shift) >= len(tileHeights)) {
 		return 0.0, errors.New("invalid height shift")
 	}
-	return (raw * tileHeights[int(shift)]) / scale, nil
+	return float32((float64(raw) * tileHeights[int(shift)]) / scale), nil
 }
