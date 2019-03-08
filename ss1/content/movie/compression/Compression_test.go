92e53453f89bb37e251a6a11427fd6ca56ecb0e4
Christian Haas <christian.haas@sevensuns.at>
2019-03-08 18:55:42 +0100

    extended encoder to handle two frames

--- ss1/content/movie/compression/Compression_test.go
+++ ss1/content/movie/compression/Compression_test.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/inkyblackness/hacked/ss1/content/movie/compression"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -28,7 +27,7 @@ func (suite *CompressionSuite) SetupTest() {
 
 }
 
-func (suite *CompressionSuite) TestSingleTile() {
+func (suite *CompressionSuite) TestOneFrameOneTile() {
 	frame0 := []byte{
 		0x01, 0x02, 0x03, 0x04,
 		0x05, 0x06, 0x07, 0x08,
@@ -36,18 +35,46 @@ func (suite *CompressionSuite) TestSingleTile() {
 		0x0D, 0x0E, 0x0F, 0x10,
 	}
 	suite.verifyCompression(4, 4, frame0)
-
 }
 
+func (suite *CompressionSuite) TestTwoFramesOneTile() {
+	frame0 := []byte{
+		0x01, 0x02, 0x03, 0x04,
+		0x05, 0x06, 0x07, 0x08,
+		0x09, 0x0A, 0x0B, 0x0C,
+		0x0D, 0x0E, 0x0F, 0x10,
+	}
+	frame1 := []byte{
+		0x20, 0x21, 0x22, 0x23,
+		0x24, 0x25, 0x26, 0x27,
+		0x28, 0x29, 0x2A, 0x2B,
+		0x2C, 0x2D, 0x2E, 0x2F,
+	}
+	suite.verifyCompression(4, 4, frame0, frame1)
+}
+
+/*
+func (suite *CompressionSuite) TestTwoTiles() {
+	frame0 := []byte{
+		0x01, 0x02, 0x03, 0x04, 0x20, 0x21, 0x22, 0x23,
+		0x05, 0x06, 0x07, 0x08, 0x24, 0x25, 0x26, 0x27,
+		0x09, 0x0A, 0x0B, 0x0C, 0x28, 0x29, 0x2A, 0x2B,
+		0x0D, 0x0E, 0x0F, 0x10, 0x2C, 0x2D, 0x2E, 0x2F,
+	}
+	suite.verifyCompression(8, 4, frame0)
+}
+*/
 func (suite *CompressionSuite) verifyCompression(width, height int, inFrames ...[]byte) {
 	suite.T().Helper()
 	encoder := compression.NewSceneEncoder(width, height)
 	for frameIndex, frame := range inFrames {
 		require.Equal(suite.T(), width*height, len(frame), fmt.Sprintf("Length of frame %d is wrong for dimension", frameIndex))
-		encoder.AddFrame(frame)
+		err := encoder.AddFrame(frame)
+		require.Nil(suite.T(), err, fmt.Sprintf("no error expected adding frame %d: %v", frameIndex, err))
 	}
-	controlWords, paletteLookup, encodedFrames := encoder.Encode()
+	controlWords, paletteLookup, encodedFrames, err := encoder.Encode()
 	require.Equal(suite.T(), len(inFrames), len(encodedFrames), "expected equal amount of encoded frames for input frames")
+	require.Nil(suite.T(), err, fmt.Sprintf("no error expected encoding: %v", err))
 
 	decoderBuilder := compression.NewFrameDecoderBuilder(width, height)
 	decoderBuilder.WithControlWords(controlWords)
--- ss1/content/movie/compression/SceneEncoder.go
+++ ss1/content/movie/compression/SceneEncoder.go
@@ -24,32 +24,64 @@ type SceneEncoder struct {
 
 // NewSceneEncoder returns a new instance.
 func NewSceneEncoder(width, height int) *SceneEncoder {
-	return &SceneEncoder{
+	e := &SceneEncoder{
 		hTiles: width / TileSideLength,
 		vTiles: height / TileSideLength,
 		stride: width,
 	}
+	e.lastFrame = make([]byte, e.vTiles*TileSideLength*e.stride)
+	return e
 }
 
 // AddFrame registers a further frame to the scene.
-func (e *SceneEncoder) AddFrame(frame []byte) {
+func (e *SceneEncoder) AddFrame(frame []byte) error {
 	var delta frameDelta
-	delta.tiles = append(delta.tiles, tileDelta{})
-	copy(delta.tiles[0][:], frame)
+	for vTile := 0; vTile < e.vTiles; vTile++ {
+		vStart := vTile * e.stride * TileSideLength
+		for hTile := 0; hTile < e.hTiles; hTile++ {
+			delta.tiles = append(delta.tiles, e.deltaTile(vStart+(hTile*TileSideLength), frame))
+		}
+	}
 	e.deltas = append(e.deltas, delta)
+
+	e.copyLastFrame(frame)
+	return nil
+}
+
+func (e *SceneEncoder) deltaTile(offset int, frame []byte) tileDelta {
+	var delta tileDelta
+	for y := 0; y < TileSideLength; y++ {
+		start := offset + (y * e.stride)
+		for x := 0; x < TileSideLength; x++ {
+			pixel := frame[start+x]
+			if pixel != e.lastFrame[start+x] {
+				delta[y*TileSideLength+x] = pixel
+			}
+		}
+	}
+	return delta
+}
+
+func (e *SceneEncoder) copyLastFrame(frame []byte) {
+	hPixel := e.hTiles * TileSideLength
+	for y := 0; y < e.vTiles*TileSideLength; y++ {
+		start := y * e.stride
+		copy(e.lastFrame[start:start+hPixel], frame[start:])
+	}
 }
 
 // Encode processes all the previously registered frames and creates the necessary components for decoding.
-func (e *SceneEncoder) Encode() (words []ControlWord, paletteLookup []byte, frames []EncodedFrame) {
-	words = append(words, ControlWordOf(12, CtrlColorTile16ColorsMasked, 0))
-	paletteLookup = e.deltas[0].tiles[0][:]
+func (e *SceneEncoder) Encode() (words []ControlWord, paletteLookup []byte, frames []EncodedFrame, err error) {
 	frames = make([]EncodedFrame, len(e.deltas))
-	frames[0].Bitstream = []byte{0x00, 0x00}
-	frames[0].Maskstream = []byte{0x10, 0x32, 0x54, 0x76, 0x98, 0xBA, 0xDC, 0xFE}
-	/*
-		for index, delta := range e.deltas {
-			frames[index]
-		}
-	*/
+	for frameIndex := 0; frameIndex < len(e.deltas); frameIndex++ {
+		outFrame := &frames[frameIndex]
+		delta := e.deltas[frameIndex]
+
+		words = append(words, ControlWordOf(12, CtrlColorTile16ColorsMasked, uint32(frameIndex*16)))
+		paletteLookup = append(paletteLookup, delta.tiles[0][:]...)
+		outFrame.Bitstream = []byte{0x00, byte(frameIndex) << 4}
+		outFrame.Maskstream = []byte{0x10, 0x32, 0x54, 0x76, 0x98, 0xBA, 0xDC, 0xFE}
+	}
+
 	return
 }
