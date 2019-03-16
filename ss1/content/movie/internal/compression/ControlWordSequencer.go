2770ac010e5a5a8a36b3e7a47cb52d9f446e1072
Christian Haas <christian.haas@sevensuns.at>
2019-03-16 21:24:38 +0100

    continuing with control word sequencer

--- ss1/content/movie/internal/compression/ControlWordSequencer.go
+++ ss1/content/movie/internal/compression/ControlWordSequencer.go
@@ -2,6 +2,7 @@ package compression
 
 import (
 	"errors"
+	"sort"
 )
 
 // TileColorOp describes one operation how a tile should be colored.
@@ -13,29 +14,50 @@ type TileColorOp struct {
 // ControlWordSequencer receives a list of requested tile coloring operations
 // and produces a sequence of low-level control words to reproduce the requested list.
 type ControlWordSequencer struct {
+	ops map[TileColorOp]uint32
 }
 
 // Add extends the list of requested coloring operations with the given entry.
 // The offset of the entry must use up to 20 bits, otherwise the function returns an error.
 func (seq *ControlWordSequencer) Add(op TileColorOp) error {
-	return errors.New("not implemented")
+	if op.Offset >= 0x100000 {
+		return errors.New("too high operation offset")
+	}
+	if seq.ops == nil {
+		seq.ops = make(map[TileColorOp]uint32)
+	}
+	seq.ops[op]++
+	return nil
 }
 
 // Sequence packs the requested list of entries into a sequence of control words.
 // An error is returned if the operations would exceed the possible storage space.
-func (seq *ControlWordSequencer) Sequence() (ControlWordSequence, error) {
-	return ControlWordSequence{}, errors.New("not implemented")
+func (seq ControlWordSequencer) Sequence() (ControlWordSequence, error) {
+	var result ControlWordSequence
+	var sortedOps []TileColorOp
+	for op := range seq.ops {
+		sortedOps = append(sortedOps, op)
+	}
+	sort.Slice(sortedOps, func(a, b int) bool {
+		return seq.ops[sortedOps[a]] > seq.ops[sortedOps[b]]
+	})
+	for _, op := range sortedOps {
+		result.words = append(result.words, ControlWordOf(12, op.Type, op.Offset))
+	}
+
+	return result, nil
 }
 
 // ControlWordSequence is a finalized set of control words to reproduce a list of
 // tile coloring operations. Based on this sequence, a bitstream can be created based
 // on a selection of such coloring operations (i.e., per frame).
 type ControlWordSequence struct {
+	words []ControlWord
 }
 
 // ControlWords returns the list of low-level control words of the sequence.
 func (seq ControlWordSequence) ControlWords() []ControlWord {
-	return nil
+	return seq.words
 }
 
 // BitstreamFor returns the bitstream to reproduce the provided list of coloring operations
---
+++ ss1/content/movie/internal/compression/ControlWordSequencer_test.go
@@ -0,0 +1,86 @@
+package compression_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/inkyblackness/hacked/ss1/content/movie/internal/compression"
+)
+
+type ControlWordSequencerSuite struct {
+	suite.Suite
+
+	sequencer compression.ControlWordSequencer
+
+	sequenceErr error
+	sequence    compression.ControlWordSequence
+}
+
+func TestControlWordSequencer(t *testing.T) {
+	suite.Run(t, new(ControlWordSequencerSuite))
+}
+
+func (suite *ControlWordSequencerSuite) SetupTest() {
+	suite.sequencer = compression.ControlWordSequencer{}
+	suite.sequenceErr = nil
+	suite.sequence = compression.ControlWordSequence{}
+}
+
+func (suite *ControlWordSequencerSuite) TestEmptySequence() {
+	suite.whenSequenceIsCreated()
+	suite.thenControlWordsShouldHaveALenOf(0)
+}
+
+func (suite *ControlWordSequencerSuite) TestAddReturnsErrorOnWrongOffset() {
+	err := suite.sequencer.Add(compression.TileColorOp{Offset: 0x100000})
+	assert.NotNil(suite.T(), err, "error expected adding an operation with too high offset")
+}
+
+func (suite *ControlWordSequencerSuite) TestSingleOperation() {
+	suite.givenRegisteredOperations(compression.TileColorOp{})
+	suite.whenSequenceIsCreated()
+	suite.thenControlWordsShouldHaveALenOf(1)
+}
+
+func (suite *ControlWordSequencerSuite) TestDuplicatedOperationShouldResultInOneControlWord() {
+	suite.givenRegisteredOperations(compression.TileColorOp{}, compression.TileColorOp{})
+	suite.whenSequenceIsCreated()
+	suite.thenControlWordsShouldHaveALenOf(1)
+}
+
+func (suite *ControlWordSequencerSuite) TestControlWordsAreOrderedByOperationFrequency() {
+	suite.givenRegisteredOperations(
+		compression.TileColorOp{Offset: 1},
+		compression.TileColorOp{}, compression.TileColorOp{})
+	suite.whenSequenceIsCreated()
+	suite.thenControlWordsShouldBe(compression.ControlWordOf(12, 0, 0), compression.ControlWordOf(12, 0, 1))
+}
+
+func (suite *ControlWordSequencerSuite) givenRegisteredOperations(ops ...compression.TileColorOp) {
+	suite.T().Helper()
+	for index, op := range ops {
+		err := suite.sequencer.Add(op)
+		require.Nil(suite.T(), err, "no error expected adding operation ", index)
+	}
+}
+
+func (suite *ControlWordSequencerSuite) whenSequenceIsCreated() {
+	suite.sequence, suite.sequenceErr = suite.sequencer.Sequence()
+}
+
+func (suite *ControlWordSequencerSuite) thenControlWordsShouldHaveALenOf(expected int) {
+	suite.T().Helper()
+	require.Nil(suite.T(), suite.sequenceErr, "no error expected in this verification")
+	words := suite.sequence.ControlWords()
+	assert.Equal(suite.T(), expected, len(words))
+}
+
+func (suite *ControlWordSequencerSuite) thenControlWordsShouldBe(expected ...compression.ControlWord) {
+	suite.T().Helper()
+	words := suite.sequence.ControlWords()
+	assert.Equal(suite.T(), len(expected), len(words), "length mismatch")
+	assert.Equal(suite.T(), expected, words, "words mismatch")
+}
