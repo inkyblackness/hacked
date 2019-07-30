c103844c4a41e1e8f03e9eeb1b12b47be77b14ad
Christian Haas <christian.haas@sevensuns.at>
2019-07-30 23:02:31 +0200

    removed unneeded cache code

--- ss1/content/movie/internal/compression/PaletteLookupGenerator.go
+++ ss1/content/movie/internal/compression/PaletteLookupGenerator.go
@@ -3,7 +3,6 @@ package compression
 import (
 	"fmt"
 	"math/bits"
-	"sync"
 )
 
 type paletteLookupEntry struct {
@@ -48,107 +47,6 @@ func (lookup *PaletteLookup) Lookup(tile tileDelta) (index int, pal []byte, mask
 type PaletteLookupGenerator struct {
 	// deltaToKey map[tileDelta]tilePaletteKey
 	keyUses map[tilePaletteKey]int
-}
-
-type nestedEntryCache struct {
-	m       sync.Mutex
-	keys    map[tilePaletteKey]struct{}
-	entries map[tilePaletteKey]*nestedEntry
-}
-
-func (cache *nestedEntryCache) query(key tilePaletteKey, found func(*nestedEntry)) {
-	go func() {
-		cache.m.Lock()
-		entry, hasEntry := cache.entries[key]
-		cache.m.Unlock()
-		if hasEntry {
-			found(entry)
-			return
-		}
-
-		entry = &nestedEntry{key: key}
-		entry.populate(cache)
-		cache.m.Lock()
-		cache.entries[key] = entry
-		cache.m.Unlock()
-		found(entry)
-	}()
-}
-
-type nestedEntry struct {
-	key    tilePaletteKey
-	nested []nestedEntry
-}
-
-func (entry nestedEntry) buffer() []byte {
-	return entry.extractBuffer(0, func(tilePaletteKey, int) {})
-}
-
-func (entry nestedEntry) byteSize() int {
-	nestedSize := 0
-	for _, nested := range entry.nested {
-		nestedSize += nested.byteSize()
-	}
-	return entry.key.size + nestedSize
-}
-
-func (entry *nestedEntry) populate(cache *nestedEntryCache) {
-	remainingKey := entry.key
-	foundSomething := true
-	keySearchSize := remainingKey.size - 1
-	for remainingKey.size > 2 && foundSomething {
-		var lastAddedKey tilePaletteKey
-		lastAddedKey, foundSomething = entry.populateRemaining(cache, remainingKey, keySearchSize)
-		remainingKey = remainingKey.without(&lastAddedKey)
-		keySearchSize = remainingKey.size
-	}
-}
-
-func (entry *nestedEntry) populateRemaining(cache *nestedEntryCache,
-	remainingKey tilePaletteKey, startSize int) (tilePaletteKey, bool) {
-	maxByteSize := 0
-	var maxNested *nestedEntry
-	keySize := startSize
-	for (keySize > 2) && (maxNested == nil) {
-		results := make(chan *nestedEntry)
-		resultsPending := 0
-		for otherKey := range cache.keys {
-			if otherKey.size == keySize && remainingKey.contains(&otherKey) {
-				resultsPending++
-				cache.query(otherKey, func(nested *nestedEntry) {
-					results <- nested
-				})
-			}
-		}
-		for resultsPending > 0 {
-			nested := <-results
-			nestedSize := nested.byteSize()
-			if nestedSize > maxByteSize {
-				maxByteSize = nestedSize
-				maxNested = nested
-			}
-			resultsPending--
-		}
-		close(results)
-		keySize--
-	}
-	if maxNested == nil {
-		return tilePaletteKey{}, false
-	}
-	entry.nested = append(entry.nested, *maxNested)
-	return maxNested.key, true
-}
-
-func (entry *nestedEntry) extractBuffer(startOffset int, marker func(tilePaletteKey, int)) []byte {
-	var nestedBuffer []byte
-	marker(entry.key, startOffset)
-	relativeOffset := 0
-	for _, nested := range entry.nested {
-		bufferPart := nested.extractBuffer(startOffset+relativeOffset, marker)
-		nestedBuffer = append(nestedBuffer, bufferPart...)
-		relativeOffset += nested.key.size
-	}
-	return entry.key.joinedBuffer(nestedBuffer)
 }
 
 // Generate creates a lookup based on all currently registered tile deltas.
@@ -250,36 +148,8 @@ func (gen *PaletteLookupGenerator) Generate() PaletteLookup {
 				delete(remainder, sizedKey)
 			}
 		}
-
-		/*
-			var keysInSize []tilePaletteKey
-			// find all keys with this current size
-			for key := range remainder {
-				if key.size == size {
-					keysInSize = append(keysInSize, key)
-				}
-			}
-
-			for _, key := range keysInSize {
-				var toRemove []tilePaletteKey
-				cache := nestedEntryCache{
-					keys:    remainder,
-					entries: make(map[tilePaletteKey]*nestedEntry),
-				}
-				nestedRoot := nestedEntry{key: key}
-				nestedRoot.populate(&cache)
-
-				bytes := nestedRoot.extractBuffer(len(lookup.buffer), func(nestedKey tilePaletteKey, offset int) {
-					toRemove = append(toRemove, nestedKey)
-					lookup.entries[nestedKey] = paletteLookupEntry{start: offset, size: nestedKey.size}
-				})
-				addToBuffer(bytes)
-				for _, key := range toRemove {
-					delete(remainder, key)
-				}
-			}
-		*/
 	}
+	// TODO: this block can be removed if remaining is always zero
 	fmt.Printf("last remaining keys: %v\n", len(remainder))
 	for key := range remainder {
 		bytes := key.buffer()
