5f3bbbd684976558bad653d365d112f176ddac48
Christian Haas <christian.haas@sevensuns.at>
2019-08-03 15:52:08 +0200

    using MaskstreamWriter

--- ss1/content/movie/internal/compression/Maskstream.go
+++ ss1/content/movie/internal/compression/Maskstream.go
@@ -1,5 +1,26 @@
 package compression
 
+import "errors"
+
+// MaskstreamWriter writes mask integers to a byte array.
+type MaskstreamWriter struct {
+	Buffer []byte
+}
+
+// Write adds the given amount of bytes from the mask to the buffer.
+func (w *MaskstreamWriter) Write(byteCount uint, mask uint64) error {
+	if byteCount > 8 {
+		return errors.New("invalid byte count")
+	}
+	if w.Buffer == nil {
+		w.Buffer = make([]byte, 0, 1024*32)
+	}
+	for b := uint(0); b < byteCount; b++ {
+		w.Buffer = append(w.Buffer, byte(mask>>(b*8)))
+	}
+	return nil
+}
+
 // MaskstreamReader reads mask integers from a byte array.
 type MaskstreamReader struct {
 	source []byte
@@ -9,15 +30,6 @@ type MaskstreamReader struct {
 // NewMaskstreamReader returns a new reader instance for given source.
 func NewMaskstreamReader(source []byte) *MaskstreamReader {
 	return &MaskstreamReader{source: source}
-}
-
-func (reader *MaskstreamReader) nextByte() (value uint64) {
-	if reader.curPos < len(reader.source) {
-		value = uint64(reader.source[reader.curPos])
-		reader.curPos++
-	}
-
-	return
 }
 
 // Read returns a mask integer of given byte length from the current position.
@@ -34,6 +46,15 @@ func (reader *MaskstreamReader) Read(bytes int) (value uint64) {
 
 	for i := 0; i < bytes; i++ {
 		value |= reader.nextByte() << uint64(8*i)
+	}
+
+	return
+}
+
+func (reader *MaskstreamReader) nextByte() (value uint64) {
+	if reader.curPos < len(reader.source) {
+		value = uint64(reader.source[reader.curPos])
+		reader.curPos++
 	}
 
 	return
--- ss1/content/movie/internal/compression/SceneEncoder.go
+++ ss1/content/movie/internal/compression/SceneEncoder.go
@@ -86,19 +86,19 @@ func (e *SceneEncoder) Encode() (words []ControlWord, paletteLookupBuffer []byte
 		err = fmt.Errorf("palette lookup is too big: %vB", len(paletteLookupBuffer))
 		return
 	}
-	frames = make([]EncodedFrame, len(e.deltas))
 
+	frames = make([]EncodedFrame, len(e.deltas))
 	for frameIndex := 0; frameIndex < len(e.deltas); frameIndex++ {
+		var maskstreamWriter MaskstreamWriter
 		outFrame := &frames[frameIndex]
 		delta := e.deltas[frameIndex]
-		controlStatistics := make(map[int]int)
 
 		lastOp := TileColorOp{Type: CtrlUnknown}
 		for tileIndex, tile := range delta.tiles {
 			var op TileColorOp
 			paletteIndex, pal, mask := paletteLookup.Lookup(tile)
 			palSize := len(pal)
-			// TODO: determine how to implement skip (and skip row)...
+
 			switch {
 			case palSize == 1 && (pal[0] == 0x00):
 				op.Type = CtrlSkip
@@ -120,46 +120,45 @@ func (e *SceneEncoder) Encode() (words []ControlWord, paletteLookupBuffer []byte
 				if palSize > 0 {
 					op.Offset |= uint32(pal[0])
 				}
-				outFrame.Maskstream = writeMaskstream(outFrame.Maskstream, 2, mask)
+
+				_ = maskstreamWriter.Write(2, mask)
 			case palSize <= 4:
 				op.Type = CtrlColorTile4ColorsMasked
 				op.Offset = uint32(paletteIndex)
-				outFrame.Maskstream = writeMaskstream(outFrame.Maskstream, 4, mask)
+				_ = maskstreamWriter.Write(4, mask)
 			case palSize <= 8:
 				op.Type = CtrlColorTile8ColorsMasked
 				op.Offset = uint32(paletteIndex)
-				outFrame.Maskstream = writeMaskstream(outFrame.Maskstream, 6, mask)
+				_ = maskstreamWriter.Write(6, mask)
 			default:
 				op.Type = CtrlColorTile16ColorsMasked
 				op.Offset = uint32(paletteIndex)
-				outFrame.Maskstream = writeMaskstream(outFrame.Maskstream, 8, mask)
+				_ = maskstreamWriter.Write(8, mask)
 			}
-			/* todo: see if this skipping could be moved to op sequencer */
+
 			if op.Type != CtrlSkip && (tileIndex%e.hTiles) != 0 && lastOp == op {
 				op = TileColorOp{Type: CtrlRepeatPrevious}
 			} else {
 				lastOp = op
 			}
-			/**/
-			controlStatistics[int(op.Type)]++
+
 			err = wordSequencer.Add(op)
 			if err != nil {
 				return nil, nil, nil, err
 			}
 			tileColorOpsPerFrame[frameIndex] = append(tileColorOpsPerFrame[frameIndex], op)
 		}
-		// fmt.Printf("Out %v control statistics: %v\n", frameIndex, controlStatistics)
+		outFrame.Maskstream = maskstreamWriter.Buffer
 	}
 
-	sequence, err := wordSequencer.Sequence()
+	wordSequence, err := wordSequencer.Sequence()
 	if err != nil {
 		return nil, nil, nil, err
 	}
-	sequence.HTiles = uint32(e.hTiles)
-	words = sequence.ControlWords()
-	paletteLookupBuffer = paletteLookup.Buffer()
+	wordSequence.HTiles = uint32(e.hTiles)
+	words = wordSequence.ControlWords()
 	for frameIndex, ops := range tileColorOpsPerFrame {
-		frames[frameIndex].Bitstream, err = sequence.BitstreamFor(ops)
+		frames[frameIndex].Bitstream, err = wordSequence.BitstreamFor(ops)
 		if err != nil {
 			return nil, nil, nil, err
 		}
@@ -175,13 +174,4 @@ func (e *SceneEncoder) createPaletteLookup() PaletteLookup {
 		}
 	}
 	return paletteLookupGenerator.Generate()
-}
-
-func writeMaskstream(s []byte, bytes int, mask uint64) []byte {
-	result := make([]byte, len(s), len(s)+bytes)
-	copy(result, s)
-	for b := 0; b < bytes; b++ {
-		result = append(result, byte(mask>>(uint(b)*8)))
-	}
-	return result
 }
