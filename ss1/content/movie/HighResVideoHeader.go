c272be10487006f5f9c19f543415668f672c01d9
Christian Haas <christian.haas@sevensuns.at>
2020-03-15 15:43:43 +0100

    removed unused files

--- ss1/content/movie/HighResVideoHeader.go
+++
@@ -1,9 +0,0 @@
-package movie
-
-// HighResVideoHeaderSize is the size, in bytes, of the header structure.
-const HighResVideoHeaderSize = 2
-
-// HighResVideoHeader is for video entries with high resolution.
-type HighResVideoHeader struct {
-	PixelDataOffset uint16
-}
--- ss1/content/movie/LowResVideoHeader.go
+++
@@ -1,9 +0,0 @@
-package movie
-
-// LowResVideoHeaderSize is the size, in bytes, of the header structure.
-const LowResVideoHeaderSize = 8
-
-// LowResVideoHeader is for video entries with low resolution
-type LowResVideoHeader struct {
-	BoundingBox [4]uint16
-}
