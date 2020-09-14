a3077a85f6c659ab389d2268262e9c5f24d4b82f
Christian Haas <christian.haas@sevensuns.at>
2020-09-14 21:12:42 +0200

    #2 refactor test for TransactionBuilder

--- ss1/edit/undoable/cmd/Transaction_test.go
+++ ss1/edit/undoable/cmd/Transaction_test.go
@@ -4,79 +4,95 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/inkyblackness/hacked/ss1/edit/undoable/cmd"
 	"github.com/inkyblackness/hacked/ss1/world"
 )
 
-type CommanderFunc func(command cmd.Command)
-
-func (f CommanderFunc) Queue(command cmd.Command) {
-	f(command)
+func TestTransactionBuilder(t *testing.T) {
+	suite.Run(t, new(TransactionBuilderSuite))
 }
 
-func TestTransactionCallsForwardTasksFromLeftToRight(t *testing.T) {
-	var txn cmd.TransactionBuilder
-	txn.Commander = CommanderFunc(func(command cmd.Command) { _ = command.Do(nil) })
-	var result []string
-	aTask := func(text string) cmd.Task {
-		return func(modder world.Modder) error {
-			result = append(result, text)
-			return nil
-		}
-	}
-	txn.Register(cmd.Forward(aTask("fa")), cmd.Forward(aTask("fb")), cmd.Reverse(aTask("ra")))
+type TransactionBuilderSuite struct {
+	suite.Suite
 
-	assert.Equal(t, []string{"fa", "fb"}, result)
+	builder cmd.TransactionBuilder
+	command cmd.Command
+	result  []string
 }
 
-func TestTransactionCallsReverseTasksFromRightToLeft(t *testing.T) {
-	var txn cmd.TransactionBuilder
-	txn.Commander = CommanderFunc(func(command cmd.Command) { _ = command.Undo(nil) })
-	var result []string
-	aTask := func(text string) cmd.Task {
-		return func(modder world.Modder) error {
-			result = append(result, text)
-			return nil
-		}
-	}
-	txn.Register(cmd.Forward(aTask("fa")), cmd.Reverse(aTask("ra")), cmd.Reverse(aTask("rb")))
-
-	assert.Equal(t, []string{"rb", "ra"}, result)
+func (suite *TransactionBuilderSuite) SetupTest() {
+	suite.builder = cmd.TransactionBuilder{Commander: suite}
+	suite.command = nil
+	suite.result = nil
 }
 
-func TestTransactionCallsNestedInForwardDirection(t *testing.T) {
-	var txn cmd.TransactionBuilder
-	txn.Commander = CommanderFunc(func(command cmd.Command) { _ = command.Do(nil) })
-	var result []string
-	aTask := func(text string) cmd.Task {
-		return func(modder world.Modder) error {
-			result = append(result, text)
-			return nil
-		}
-	}
-	txn.Register(
-		cmd.Forward(aTask("fa")),
-		cmd.Nested(func() { txn.Register(cmd.Forward(aTask("na"))) }),
-		cmd.Forward(aTask("fb")))
-
-	assert.Equal(t, []string{"fa", "na", "fb"}, result)
+func (suite *TransactionBuilderSuite) TestTransactionCallsForwardTasksFromLeftToRight() {
+	suite.givenRegisteredTransaction(
+		cmd.Forward(suite.aTask("fa")),
+		cmd.Forward(suite.aTask("fb")),
+		cmd.Reverse(suite.aTask("ra")),
+	)
+	suite.whenCommandIsDone()
+	suite.thenResultShouldBe([]string{"fa", "fb"})
 }
 
-func TestTransactionCallsNestedInReverseDirection(t *testing.T) {
-	var txn cmd.TransactionBuilder
-	txn.Commander = CommanderFunc(func(command cmd.Command) { _ = command.Undo(nil) })
-	var result []string
-	aTask := func(text string) cmd.Task {
-		return func(modder world.Modder) error {
-			result = append(result, text)
-			return nil
-		}
-	}
-	txn.Register(
-		cmd.Reverse(aTask("ra")),
-		cmd.Nested(func() { txn.Register(cmd.Reverse(aTask("na")), cmd.Reverse(aTask("nb"))) }),
-		cmd.Reverse(aTask("rb")))
+func (suite *TransactionBuilderSuite) TestTransactionCallsReverseTasksFromRightToLeft() {
+	suite.givenRegisteredTransaction(
+		cmd.Forward(suite.aTask("fa")),
+		cmd.Reverse(suite.aTask("ra")),
+		cmd.Reverse(suite.aTask("rb")),
+	)
+	suite.whenCommandIsUndone()
+	suite.thenResultShouldBe([]string{"rb", "ra"})
+}
 
-	assert.Equal(t, []string{"rb", "nb", "na", "ra"}, result)
+func (suite *TransactionBuilderSuite) TestTransactionCallsNestedInForwardDirection() {
+	suite.givenRegisteredTransaction(
+		cmd.Forward(suite.aTask("fa")),
+		cmd.Nested(func() { suite.builder.Register(cmd.Forward(suite.aTask("na"))) }),
+		cmd.Forward(suite.aTask("fb")),
+	)
+	suite.whenCommandIsDone()
+	suite.thenResultShouldBe([]string{"fa", "na", "fb"})
+}
+
+func (suite *TransactionBuilderSuite) TestTransactionCallsNestedInReverseDirection() {
+	suite.givenRegisteredTransaction(
+		cmd.Reverse(suite.aTask("ra")),
+		cmd.Nested(func() { suite.builder.Register(cmd.Reverse(suite.aTask("na")), cmd.Reverse(suite.aTask("nb"))) }),
+		cmd.Reverse(suite.aTask("rb")),
+	)
+	suite.whenCommandIsUndone()
+	suite.thenResultShouldBe([]string{"rb", "nb", "na", "ra"})
+}
+
+func (suite *TransactionBuilderSuite) givenRegisteredTransaction(modifier ...cmd.TransactionModifier) {
+	suite.builder.Register(modifier...)
+}
+
+func (suite *TransactionBuilderSuite) whenCommandIsDone() {
+	_ = suite.command.Do(nil)
+}
+
+func (suite *TransactionBuilderSuite) whenCommandIsUndone() {
+	_ = suite.command.Undo(nil)
+}
+
+func (suite *TransactionBuilderSuite) thenResultShouldBe(expected []string) {
+	assert.Equal(suite.T(), expected, suite.result)
+}
+
+func (suite *TransactionBuilderSuite) Queue(command cmd.Command) {
+	require.Nil(suite.T(), suite.command, "Not expecting a second command")
+	suite.command = command
+}
+
+func (suite *TransactionBuilderSuite) aTask(text string) cmd.Task {
+	return func(modder world.Modder) error {
+		suite.result = append(suite.result, text)
+		return nil
+	}
 }
