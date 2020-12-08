a60566634e21ac68fd47733a8bd751977b263dd7
Christian Haas <christian.haas@sevensuns.at>
2020-12-08 19:26:40 +0100

    add more filepath support functions

--- ss1/world/FileLocation.go
+++ ss1/world/FileLocation.go
@@ -1,6 +1,9 @@
 package world
 
-import "path/filepath"
+import (
+	"path/filepath"
+	"strings"
+)
 
 // FileLocation describes the path and name of a file.
 type FileLocation struct {
@@ -8,11 +11,35 @@ type FileLocation struct {
 	Name    string
 }
 
+// FileLocationFrom returns an instance for given filename.
+func FileLocationFrom(filename string) FileLocation {
+	return FileLocation{
+		DirPath: filepath.Dir(filename),
+		Name:    filepath.Base(filename),
+	}
+}
+
 // AbsolutePathFrom returns the complete path, based on given base, of the location.
 func (loc FileLocation) AbsolutePathFrom(base string) string {
 	rel, err := filepath.Rel(base, loc.DirPath)
 	if err != nil {
-		return filepath.Join(base, loc.Name)
+		return filepath.Join(base, loc.DirPath, loc.Name)
 	}
 	return filepath.Join(base, rel, loc.Name)
+}
+
+// NestedRelativeTo returns a string that describes the relative nested location to given base - if possible.
+func (loc FileLocation) NestedRelativeTo(base string) string {
+	absoluteDir := loc.DirPath
+	if !filepath.IsAbs(absoluteDir) {
+		absoluteDir = filepath.Clean(filepath.Join(base, loc.DirPath))
+	}
+	if !strings.HasPrefix(absoluteDir+string(filepath.Separator), base+string(filepath.Separator)) {
+		return filepath.Join(absoluteDir, loc.Name)
+	}
+	rel, err := filepath.Rel(base, absoluteDir)
+	if err != nil {
+		return filepath.Join(base, loc.Name)
+	}
+	return filepath.Join(".", rel, loc.Name)
 }
--- ss1/world/FileLocation_test.go
+++ ss1/world/FileLocation_test.go
@@ -50,12 +50,75 @@ func TestFileLocationAbsolutePathFrom(t *testing.T) {
 			base:     base,
 			expected: filepath.Join(base, "nested", "test.res"),
 		},
+		{
+			name:     "nested relative file dir path",
+			loc:      world.FileLocation{DirPath: "nested", Name: "test.res"},
+			base:     base,
+			expected: filepath.Join(base, "nested", "test.res"),
+		},
 	}
 	for _, tc := range tt {
 		func(name string, loc world.FileLocation, base, expected string) {
 			t.Run(name, func(t *testing.T) {
 				abs := loc.AbsolutePathFrom(base)
 				assert.Equal(t, expected, abs)
+			})
+		}(tc.name, tc.loc, tc.base, tc.expected)
+	}
+}
+
+func TestFileLocationNestedRelativeTo(t *testing.T) {
+	temp := os.TempDir()
+	base := filepath.Join(temp, "base")
+
+	tt := []struct {
+		name     string
+		loc      world.FileLocation
+		base     string
+		expected string
+	}{
+		{
+			name:     "file loaded in same base",
+			loc:      world.FileLocation{DirPath: base, Name: "test.res"},
+			base:     base,
+			expected: filepath.Join(".", "test.res"),
+		},
+		{
+			name:     "new file",
+			loc:      world.FileLocation{DirPath: ".", Name: "test.res"},
+			base:     base,
+			expected: filepath.Join(".", "test.res"),
+		},
+		{
+			name:     "new file without dir path",
+			loc:      world.FileLocation{DirPath: "", Name: "test.res"},
+			base:     base,
+			expected: filepath.Join(".", "test.res"),
+		},
+		{
+			name:     "file found in different path",
+			loc:      world.FileLocation{DirPath: filepath.Join(temp, "other"), Name: "test.res"},
+			base:     base,
+			expected: filepath.Join(temp, "other", "test.res"),
+		},
+		{
+			name:     "file found in sub path",
+			loc:      world.FileLocation{DirPath: filepath.Join(base, "nested"), Name: "test.res"},
+			base:     base,
+			expected: filepath.Join(".", "nested", "test.res"),
+		},
+		{
+			name:     "file found in similar named different path",
+			loc:      world.FileLocation{DirPath: filepath.Join(temp, "baseOther"), Name: "test.res"},
+			base:     base,
+			expected: filepath.Join(temp, "baseOther", "test.res"),
+		},
+	}
+	for _, tc := range tt {
+		func(name string, loc world.FileLocation, base, expected string) {
+			t.Run(name, func(t *testing.T) {
+				rel := loc.NestedRelativeTo(base)
+				assert.Equal(t, expected, rel)
 			})
 		}(tc.name, tc.loc, tc.base, tc.expected)
 	}
