25946238293010f81669ac0182cabdb9f00297bb
Christian Haas <christian.haas@sevensuns.at>
2020-12-28 15:44:19 +0100

    move test to _test package

--- ss1/content/object/Descriptors_test.go
+++ ss1/content/object/Descriptors_test.go
@@ -1,29 +1,31 @@
-package object
+package object_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/inkyblackness/hacked/ss1/content/object"
 )
 
 func TestClassDescriptorTotalDataLengthReturnsCompleteLength(t *testing.T) {
-	var mainDesc ClassDescriptor
+	var mainDesc object.ClassDescriptor
 
 	mainDesc.GenericDataSize = 7
 	mainDesc.Subclasses = append(mainDesc.Subclasses,
-		SubclassDescriptor{TypeCount: 2, SpecificDataSize: 3},
-		SubclassDescriptor{TypeCount: 1, SpecificDataSize: 20})
+		object.SubclassDescriptor{TypeCount: 2, SpecificDataSize: 3},
+		object.SubclassDescriptor{TypeCount: 1, SpecificDataSize: 20})
 
-	assert.Equal(t, (7*(2+1))+(2*3)+(1*20)+CommonPropertiesSize*3, mainDesc.TotalDataSize())
+	assert.Equal(t, (7*(2+1))+(2*3)+(1*20)+object.CommonPropertiesSize*3, mainDesc.TotalDataSize())
 }
 
 func TestClassDescriptorTotalTypeCountReturnsTotalAmount(t *testing.T) {
-	var mainDesc ClassDescriptor
+	var mainDesc object.ClassDescriptor
 
 	mainDesc.GenericDataSize = 7
 	mainDesc.Subclasses = append(mainDesc.Subclasses,
-		SubclassDescriptor{TypeCount: 2, SpecificDataSize: 3},
-		SubclassDescriptor{TypeCount: 1, SpecificDataSize: 20})
+		object.SubclassDescriptor{TypeCount: 2, SpecificDataSize: 3},
+		object.SubclassDescriptor{TypeCount: 1, SpecificDataSize: 20})
 
 	assert.Equal(t, 3, mainDesc.TotalTypeCount())
 }
